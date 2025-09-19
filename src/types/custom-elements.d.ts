f5707e51c975c17e44ed018467e33206923a0fbf
oomavera <oomavera1@gmail.com>
2025-09-19 15:11:36 -0400

    chore(lint): replace explicit any with safer types for Wistia element

--- src/types/custom-elements.d.ts
+++ src/types/custom-elements.d.ts
@@ -1,7 +1,7 @@
 // Allow JSX to accept the Wistia custom element used by the hosted player
 declare namespace JSX {
   interface IntrinsicElements {
-    'wistia-player': any;
+    'wistia-player': Record<string, unknown>;
   }
 }
 
